2c2460f5009408d497b715ddd4bb4cd5d088481e
CatherineP37 <137174676+CatherineP37@users.noreply.github.com>
2026-04-15 13:54:27 +0200

    Add static folder and static files

--- events_calendar/events_calendar/settings.py
+++ events_calendar/events_calendar/settings.py
@@ -17,7 +17,6 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-
 # Application definition
 
 INSTALLED_APPS = [
@@ -106,3 +105,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = ['static/']
