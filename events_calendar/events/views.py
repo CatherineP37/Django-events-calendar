43d3922eec7573f102ef570a4a270857e66b0671
CatherineP37 <137174676+CatherineP37@users.noreply.github.com>
2026-04-15 14:14:05 +0200

    Add event_details.html template and event_details view

--- events_calendar/events/views.py
+++ events_calendar/events/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import HttpResponseRedirect
+from django.http import HttpResponse
+from django.views import generic
+from django.utils.safestring import mark_safe
+from datetime import timedelta, datetime, date
+import calendar
+from django.urls import reverse
+from django.shortcuts import get_object_or_404
+from events.models import Event
+from events.utils import Calendar
 
-# Create your views here.
+def event_details(request, event_id):
+    event = Event.objects.get(id=event_id)
+    context = {"event": event}
+    return render(request, 'events/event-details.html', context)
