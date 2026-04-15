8c2d12d74d6ca2c37ea50e65a786126d4b5027fb
CatherineP37 <137174676+CatherineP37@users.noreply.github.com>
2026-04-15 14:07:14 +0200

    Add functions to Event model in models.py

--- events_calendar/events/models.py
+++ events_calendar/events/models.py
@@ -5,3 +5,14 @@ from django.urls import reverse
 class Event(models.Model):
     title = models.CharField(max_length=200)
     start_time = models.DateTimeField()
+
+    def __str__(self):
+        return self.title
+    
+    def get_absolute_url(self):
+        return reverse("events:event-detail", args=(self.id,))
+    
+    @property
+    def get_html_url(self):
+        url = reverse("events:event_details", args=(self.id,))
+        return f'<a href="{url}">{self.title}</a>'
