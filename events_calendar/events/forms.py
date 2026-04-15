c9096ad91028bb4479cbcb043fdd81ada471515d
CatherineP37 <137174676+CatherineP37@users.noreply.github.com>
2026-04-15 14:24:31 +0200

    Add EventForm to forms.py

---
+++ events_calendar/events/forms.py
@@ -0,0 +1,21 @@
+from django.forms import ModelForm, DateInput
+from events.models import Event
+from django import forms
+
+class EventForm(ModelForm):
+    class Meta:
+        model = Event
+        fields = ["title", "start_time"]
+        widgets = {
+            "title": forms.TextInput(
+                attrs={"placeholder": "Enter event title"}
+            ),
+            "start_time": DateInput(
+                attrs={"type": "datetime-local",},
+                format="%Y-%m-%dT%H:%M",
+            ),
+        }
+
+        def __init__(self, *args, **kwargs):
+            super(EventForm, self).__init__(*args, **kwargs)       
+            self.fields["start_time"].input_formats = ("%Y-%m-%dT%H:%M",)
